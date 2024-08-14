ecfdeb67ff57b344fe1d9fa9b260bf8d965b1d5c
glasshalfpool <glasshalfpool@gmail.com>
2024-08-14 14:45:21 +0100

    Moved styles
    Styles for swiper need to be inline so moved to main file

--- related-content.php
+++ related-content.php
@@ -37,7 +37,99 @@ function enqueue_related_content_styles() {
     wp_enqueue_style( 'related-content-external-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css');
 
     // Add inline CSS
-    $custom_css = "";
+    $custom_css = ".photographer-swiper-container {
+      padding-top: var(--wp--preset--spacing--medium);
+    }
+    
+    .swiper {
+      margin-left: 0;
+      margin-right: 0;     
+      padding-bottom: 30px;
+    }
+    
+    .swiper-button-next::after, .swiper-button-prev::after {
+      color: #f5eb4a;
+      font-size: 60px;
+    }
+    
+    .mySwiper:hover {
+      cursor: pointer;
+    }
+    
+    .photographer-swiper-container .swiper-slide {      
+      max-height: 100vh;
+      max-width: 90%;
+    }  
+    
+    .content-type-story-carousel .swiper-slide {
+      max-width: 300px;
+    }
+    
+    .content-type-story-carousel .swiper-slide:hover {
+      cursor: pointer;
+    }
+    
+    .swiper-slide-image-container {
+      height: 80%;
+    }
+    
+    .photographer-swiper-container .swiper-slide img {      
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+      max-height: 80vh;
+    }
+    
+    .related-content-section.content-type-story-carousel .swiper-slide a {
+      text-decoration: none;
+    }
+    
+    .related-content-section.content-type-story-carousel .swiper-slide img {
+      width: 100%;
+        height: 100%;
+        object-fit: cover;
+      aspect-ratio: 4/3;
+    }
+    
+    .swiper-slide p {
+      font-size: var(--wp--preset--font-size--x-small);
+      color: var(--wp--preset--color--charcoal);
+      flex-basis: 20%;
+      max-width: 400px;      
+    }
+    
+    .related-content-section.content-type-story-carousel .swiper-slide h3 {
+      font-weight: bold;
+      font-size: var(--wp--preset--font-size--small);
+    }
+    
+    .swiper-slide {
+      width: auto;
+    }
+    
+    .swiper-control-arrows {
+      font-weight: bold;
+      cursor: pointer;
+      display: flex;
+      justify-content: right;
+      font-size: var(--wp--preset--font-size--medium);
+    }
+    
+    .swiper-control-arrows .swiper-button-disabled {
+      opacity: 0.35;
+    }
+    
+    .read-story-link {
+      font-size: var(--wp--preset--font-size--x-small);
+    }
+    
+    @media (max-width: 800px) {
+    
+      .swiper-slide p {        
+      max-width: 300px;        
+      }
+    
+    }";
     wp_add_inline_style( 'related-content-external-css', $custom_css );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_related_content_styles' );
